e6227aef50f1b14fcbb5d0dfbe3964e89701f96b
root <root@li764-100.(none)>
2017-07-16 00:45:54 +0800

    4.12.1

--- kernel-upgrade-centos6-x64.sh
+++ kernel-upgrade-centos6-x64.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 echo "install the kernel & headers ..."
-echo "...... version 4.12.0 ......"
+echo "...... version 4.12.1 ......"
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-yum install -y https://github.com/malaohu/ruyo-shell/raw/master/CentOS6-X64-Kernel/4.12.0/kernel-ml-4.12.0-1.el6.elrepo.x86_64.rpm
+yum install -y https://github.com/malaohu/ruyo-shell/raw/master/CentOS6-X64-Kernel/4.12.1/kernel-ml-4.12.1-1.el6.elrepo.x86_64.rpm
 yum remove -y kernel-headers
-yum install -y https://github.com/malaohu/ruyo-shell/raw/master/CentOS6-X64-Kernel/4.12.0/kernel-ml-headers-4.12.0-1.el6.elrepo.x86_64.rpm
-yum install -y https://github.com/malaohu/ruyo-shell/raw/master/CentOS6-X64-Kernel/4.12.0/kernel-ml-devel-4.12.0-1.el6.elrepo.x86_64.rpm
+yum install -y https://github.com/malaohu/ruyo-shell/raw/master/CentOS6-X64-Kernel/4.12.1/kernel-ml-headers-4.12.1-1.el6.elrepo.x86_64.rpm
+yum install -y https://github.com/malaohu/ruyo-shell/raw/master/CentOS6-X64-Kernel/4.12.1/kernel-ml-devel-4.12.1-1.el6.elrepo.x86_64.rpm
 echo "change grub info ..."
 sed -ri 's/default=1/default=0/g' /etc/grub.conf
 echo "restart ..."
