98d5a2492095c00f8a382cb486e5a5c9c8ef2ee0
malaohu <tua@live.cn>
2020-03-19 19:40:17 +0800

    Update install_ssh.sh

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-read -t 30 -p "版本号: " v
+read -t 130 -p "版本号: " v
 echo -e "\n"
 
-wget ftp://192.99.11.204/openssh-$vp1.tar.gz
-tar -zxvf openssh-$vp1.tar.gz
+wget ftp://192.99.11.204/openssh-${v}p1.tar.gz
+tar -zxvf openssh-${v}p1.tar.gz
 cd openssh-8.2p1
 ./configure --prefix=/usr --sysconfdir=/etc/ssh --with-pam --with-zlib --with-md5-passwords --with-tcp-wrappers
 make && make install
